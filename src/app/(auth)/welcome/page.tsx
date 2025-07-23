f707e1071d5237ef180c90085d299572c40949e8
devmilek <devmilek@gmail.com>
2025-07-23 17:01:03 +0200

    feat: add LoadingSpinner component and integrate it into the welcome page

--- src/app/(auth)/welcome/page.tsx
+++ src/app/(auth)/welcome/page.tsx
@@ -3,6 +3,7 @@
 // synchronize auth status to database
 
 import { Heading } from "@/components/heading";
+import { LoadingSpinner } from "@/components/loading-spinner";
 // import { LoadingSpinner } from "@/components/loading-spinner";
 import { client } from "@/lib/client";
 import { useQuery } from "@tanstack/react-query";
@@ -33,7 +34,7 @@ const Page = () => {
       <BackgroundPattern className="absolute inset-0 left-1/2 z-0 -translate-x-1/2 opacity-75" />
 
       <div className="relative z-10 flex -translate-y-1/2 flex-col items-center gap-6 text-center">
-        {/* <LoadingSpinner size="md" /> */}
+        <LoadingSpinner size="md" />
         <Heading>Creating your account...</Heading>
         <p className="text-base/7 text-gray-600 max-w-prose">
           Just a moment while we set things up for you.
