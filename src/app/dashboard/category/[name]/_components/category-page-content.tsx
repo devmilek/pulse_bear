5d3def79f6ebd39ed9e8b558f330e76ee417db4b
devmilek <devmilek@gmail.com>
2025-07-29 21:05:03 +0200

    refactor: remove unused imports in CategoryPageContent component

--- src/app/dashboard/category/[name]/_components/category-page-content.tsx
+++ src/app/dashboard/category/[name]/_components/category-page-content.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
 import { EventCategory } from "@/db/schema";
 import { EmptyCategoryState } from "./empty-category-state";
-import { EventsTabsSection } from "./events-tabs-section";
-import { useEventCategoryParams } from "@/hooks/use-event-category-params";
 import { EventsStatsGrid } from "./events-stats-grid";
-import { EventsDataTable } from "./events-data-table";
-import { useTRPC } from "@/trpc/client";
-import { EventsList } from "./events-list";
 import { EventsFeed } from "./events-feed";
 import { EventsChart } from "./events-chart";
-import { ChartAreaInteractive } from "./chart-area-interactive";
 
 interface CategoryPageContentProps {
   hasEvents: boolean;
